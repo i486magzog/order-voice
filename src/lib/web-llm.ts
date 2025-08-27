19eaf1d4fb6f19fb73e56d43d7c2abdac436b46f
Shinkee Cho <development@magzog.com>
2025-08-28 08:39:30 +1200

    Refactor

--- src/lib/web-llm.ts
+++ src/lib/web-llm.ts
@@ -1,4 +1,7 @@
+'use client'
+
 import { Order } from "@/shared/types/global";
+// import { CreateMLCEngine } from "@mlc-ai/web-llm";
 
 export class WebLLM {
   async makeSpeechText(order: Order): Promise<string> {
@@ -7,3 +10,14 @@ export class WebLLM {
     return base;
   }
 }
+
+// // Callback function to update model loading progress
+// const initProgressCallback = (initProgress: any) => {
+//   console.log(initProgress);
+// }
+// const selectedModel = "Llama-3.1-8B-Instruct-q4f32_1-MLC";
+
+// const engine = await CreateMLCEngine(
+//   selectedModel,
+//   { initProgressCallback: initProgressCallback }, // engineConfig
+// );
